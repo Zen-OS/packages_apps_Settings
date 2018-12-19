319caa3c2ca737f2887a49cbfb2a74925ce88088
wirmpolter <wirmpolter@gmail.com>
2018-12-19 18:03:09 -0500

    Settings: Suppress unused source files warning
    Change-Id: Ie2166a9cdcaf3d3f8dd164e9a4607152d8ec9a74

--- Android.mk
+++ Android.mk
@@ -18,8 +18,10 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
 LOCAL_USE_AAPT2 := true
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src) \
-LOCAL_SRC_FILES += $(call all-java-files-under, ../Chi/src)
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, src) \
+    $(call all-java-files-under, ../Chi/src)
+
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-slices-builders \
     android-slices-core \
